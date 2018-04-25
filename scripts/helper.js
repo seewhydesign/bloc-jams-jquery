69ba710e3022606f7856723e74bcb8644995e9a7
Cindy <seewhydesign8@gmail.com>
2018-04-25 13:34:56 -0400

    Assignment Sliders: completed

---
+++ scripts/helper.js
@@ -0,0 +1,9 @@
+class Helper {
+  playPauseAndUpdate (song) {
+    player.playPause(song);
+    const duration = player.currentlyPlaying.duration;
+    $('#time-control .total-time').text( duration );
+  }
+}
+
+const helper = new Helper();
--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,7 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    // player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
@@ -13,7 +14,8 @@
     if (currentSongIndex === 0) { return; }
 
     const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
+    // player.playPause(previousSong);
+    helper.playPauseAndUpdate(previousSong);
   });
 
 // enable next track button
@@ -25,11 +27,17 @@
     if (nextSongIndex >= album.songs.length) { return; }
 
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    // player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
   });
 
   $('#time-control input').on('input', function (event) {
     player.skipTo(event.target.value);
+  });
+
+// added for volume control
+  $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
   });
 
   setInterval( () => {
@@ -40,5 +48,13 @@
     $('#time-control .current-time').text( currentTime );
     $('#time-control input').val(percent);
   }, 1000);
+
+
+// event handler for volume
+  setVolume( () => {
+    if (player.playState !== 'playing') { return; }
+    $('#volume-control input').val(value);
+  });
+
 
 }
